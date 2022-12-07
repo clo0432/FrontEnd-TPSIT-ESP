13aeb66228bfe591a3b52d6f926f277158dfc065
clo0432 <superclaudiolodi@gmail.com>
2022-12-07 10:38:00 +0100

    Parse e memoria dei dati inseriti dall'user

--- react-app/src/scenes/pages/mainPage/index.jsx
+++ react-app/src/scenes/pages/mainPage/index.jsx
@@ -1,25 +1,51 @@
+import { useState } from 'react';
+
 const MainPage = () => {
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setInputs(values => ({...values, [name]: value}));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(inputs["scuola"] && inputs["piano"])
+            console.log(inputs);
+        else
+            console.log("dirty form");
+    };
+
     return (
         <div>
             <nav>
-
-
                 <button>Options</button>
-
             </nav>
             <main>
                 <h1>Welcome</h1>
                 <p>Breve descrizione</p>
 
-                <form>
-                    <label htmlFor="scuole">Scuola:</label>
-                    <select id="scuole">
-                        <option>Alberghetti</option>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="scuola">Scuola:</label>
+                    <select
+                        id="scuola"
+                        name="scuola"
+                        onChange={handleChange}
+                    >
+                        <option value={""}></option>
+                        <option value={"Albergehtti"}>Alberghetti</option>
                     </select>
 
                     <label htmlFor="piano">Piano:</label>
-                    <select id="piano">
-                        <option>1</option>
+                    <select
+                        id="piano"
+                        name="piano"
+                        onChange={handleChange}
+                    >
+                        <option value={""}></option>
+                        <option value={1}>1</option>
                     </select>
 
                     <button>Vai</button>
