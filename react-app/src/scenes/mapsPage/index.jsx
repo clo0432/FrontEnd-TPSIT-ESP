0d2abf6feddbe253f5f4b550f855d6856cc095ff
clo0432 <superclaudiolodi@gmail.com>
2022-12-14 09:49:41 +0100

    Correggere le coordinate, possibile idea -> prendere attuali windows height e windows width e correggere di conseguenza

--- react-app/src/scenes/mapsPage/index.jsx
+++ react-app/src/scenes/mapsPage/index.jsx
@@ -1,14 +1,30 @@
-import { useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useState } from 'react';
+
+import mappaAlberghetti from "./pianta alberghetti_piano terra.jpg"
+
 
 const MapsPage = () => {
     const location = useLocation();
-
-    console.log(location.state);
+    const navigate = useNavigate();
+    const [coordinate, setCoordinate] = useState([34, 303, 546, 815]);
+    
+    const handleRegioni = (e) => {
+        navigate(
+            "../graphs"
+        );
+    }
     
     return (
         <>
             <h1>Maps Page</h1>
             <p>{location.state.scuola}</p>
+
+            <img alt="Esempio di immagine con mappe" src={mappaAlberghetti}
+                border="1" usemap="#aree" />
+            <map name="aree" id="aree">
+                <area shape="rect" coords={coordinate} bg-color="red" onClick={handleRegioni} target="_blank" alt="Valle d'Aosta" />
+            </map>
         </>
     );
 }
