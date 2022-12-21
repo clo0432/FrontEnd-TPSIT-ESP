d2dd5991082245bfabec864b5134ff38b87667dc
clo0432 <superclaudiolodi@gmail.com>
2022-12-21 09:49:08 +0100

    Messe a posto le coordinate guardare per prendere le dimensioni dell'immagine importata

--- react-app/src/scenes/mapsPage/index.jsx
+++ react-app/src/scenes/mapsPage/index.jsx
@@ -1,30 +1,49 @@
 import { useNavigate, useLocation } from "react-router-dom";
-import { useState } from 'react';
+import React, { useRef, useEffect } from 'react';
+import { useState } from "react";
 
 import mappaAlberghetti from "./pianta alberghetti_piano terra.jpg"
-
 
 const MapsPage = () => {
     const location = useLocation();
     const navigate = useNavigate();
-    const coordinate = [34, 303, 546, 815];
 
-    const imgWidth = Math.floor(0.8 * window.screen.width);
-    const imgHeight = Math.floor(imgWidth / 1.43);
+    const [startDimensions, setStartDimensions] = useState({ width: 0, height: 0 });
+    const [ratio, SetRatio] = useState();
+    const imageRef = useRef();
+
+    let coordinate = [34, 303, 546, 815];
 
     const handleClick = (e) => {
-        navigate(
+        /*navigate(
             "../graphs"
-        );
+        );*/
+        console.log("click");
     }
-    
+
+    imgWidth = Math.floor(0.8 * window.screen.width);
+    imgHeight = Math.floor(imgWidth / ratio);
+
+    coordinate[0] = Math.floor(
+        (coordinate[0] * imgWidth) / startDimensions.width
+    );
+    coordinate[1] = Math.floor(
+        (coordinate[1] * imgHeight) / startDimensions.height
+    );
+    coordinate[2] = Math.floor(
+        (coordinate[2] * imgWidth) / startDimensions.width
+    );
+    coordinate[3] = Math.floor(
+        (coordinate[3] * imgHeight) / startDimensions.height
+    );
+
     return (
         <>
             <h1>Maps Page</h1>
             <p>{location.state.scuola}</p>
 
-            <img alt="Esempio di immagine con mappe" src={mappaAlberghetti}
-                border="1" usemap="#aree" width={imgWidth} height={imgHeight} />
+            <img ref={imageRef} src={mappaAlberghetti} alt="Mappa dell'albergo" />
+            <img alt="Esempio di immagine con mappe" src={mappaAlberghetti} useMap="#aree" width={imgWidth} height={imgHeight} />
             <map name="aree" id="aree">
                 <area shape="rect" coords={coordinate} onClick={handleClick} target="_blank" alt="Valle d'Aosta" />
             </map>
