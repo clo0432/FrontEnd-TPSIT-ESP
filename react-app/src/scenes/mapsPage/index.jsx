6d086d5d3bd9973498c2407d21adbde2bbf8a6af
Claudio <superclaudiolodi@gmail.com>
2022-12-22 22:33:36 +0100

    Anzichè avere le dimensioni dell'immagine hardcoded le dimensioni intrinsiche le ricavo una volta che l'immagine è stata caricata #2

--- react-app/src/scenes/mapsPage/index.jsx
+++ react-app/src/scenes/mapsPage/index.jsx
@@ -1,51 +1,60 @@
-import { useNavigate, useLocation } from "react-router-dom";
-import React, { useRef, useEffect } from 'react';
 import { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 import mappaAlberghetti from "./pianta alberghetti_piano terra.jpg"
 
 const MapsPage = () => {
     const location = useLocation();
     const navigate = useNavigate();
-
-    const [startDimensions, setStartDimensions] = useState({ width: 0, height: 0 });
-    const [ratio, SetRatio] = useState();
-    const imageRef = useRef();
-
+    
+    const [isLoaded, setIsLoaded] = useState(false);
+    
     let coordinate = [34, 303, 546, 815];
+
+    const onImageLoad = ({target:img}) => {
+        const startDimensions = {
+            width: img.offsetWidth, 
+            height: img.offsetHeight 
+        };
+        const ratio = startDimensions.width / startDimensions.height;
+
+        const imgWidth = Math.floor(0.8 * window.screen.width);
+        const imgHeight = Math.floor(imgWidth / ratio);
+
+        img.setAttribute('width', imgWidth);
+        img.setAttribute('height', imgHeight);
+
+        coordinate[0] = Math.floor(
+            (coordinate[0] * imgWidth) / startDimensions.width
+        );
+        coordinate[1] = Math.floor(
+            (coordinate[1] * imgHeight) / startDimensions.height
+        );
+        coordinate[2] = Math.floor(
+            (coordinate[2] * imgWidth) / startDimensions.width
+        );
+        coordinate[3] = Math.floor(
+            (coordinate[3] * imgHeight) / startDimensions.height
+        );
+
+        setIsLoaded(true);
+    };
 
     const handleClick = (e) => {
         /*navigate(
             "../graphs"
         );*/
         console.log("click");
-    }
-
-    imgWidth = Math.floor(0.8 * window.screen.width);
-    imgHeight = Math.floor(imgWidth / ratio);
-
-    coordinate[0] = Math.floor(
-        (coordinate[0] * imgWidth) / startDimensions.width
-    );
-    coordinate[1] = Math.floor(
-        (coordinate[1] * imgHeight) / startDimensions.height
-    );
-    coordinate[2] = Math.floor(
-        (coordinate[2] * imgWidth) / startDimensions.width
-    );
-    coordinate[3] = Math.floor(
-        (coordinate[3] * imgHeight) / startDimensions.height
-    );
+    };
 
     return (
         <>
             <h1>Maps Page</h1>
             <p>{location.state.scuola}</p>
-
-            <img ref={imageRef} src={mappaAlberghetti} alt="Mappa dell'albergo" />
-            <img alt="Esempio di immagine con mappe" src={mappaAlberghetti} useMap="#aree" width={imgWidth} height={imgHeight} />
+            
+            <img alt="Esempio di immagine con mappe" onLoad={onImageLoad} src={mappaAlberghetti} useMap="#aree" style={isLoaded ? { display: 'block' } : { display: 'none'}} />
             <map name="aree" id="aree">
-                <area shape="rect" coords={coordinate} onClick={handleClick} target="_blank" alt="Valle d'Aosta" />
+                <area shape="rect" coords={coordinate} onClick={handleClick} alt="area" />
             </map>
         </>
     );
